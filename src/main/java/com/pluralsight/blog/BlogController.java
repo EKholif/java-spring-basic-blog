9ff3d7739140841c867d5c7cf888003192540cfa
ehab <ehabkholif@gmail.com>
2023-03-04 21:14:11 -0500

    Completed module

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -1,8 +1,21 @@
 package com.pluralsight.blog;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc
 @Controller
 public class BlogController {
+
+    @RequestMapping("/")
+    public String listPosts(ModelMap map){
+        map.put("title" , "Blog Post 1");
+        return "home";
+
+    }
 
 }
