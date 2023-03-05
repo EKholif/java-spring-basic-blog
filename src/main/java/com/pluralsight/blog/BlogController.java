156f917a5fa295233049a67047173bb32ca521df
ehab <ehabkholif@gmail.com>
2023-03-05 00:00:28 -0500

    Completed module

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -1,5 +1,8 @@
 package com.pluralsight.blog;
 
+import com.pluralsight.blog.data.PostRepository;
+import com.pluralsight.blog.model.Post;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,15 +10,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+import java.util.List;
+
+
 @EnableWebMvc
 @Controller
 public class BlogController {
+@Autowired
+private PostRepository postRepository ;
 
-    @RequestMapping("/")
-    public String listPosts(ModelMap map){
-        map.put("title" , "Blog Post 1");
-        return "home";
-
+    public BlogController(PostRepository postRepository) {
+        this.postRepository = postRepository;
     }
 
+    @RequestMapping("/")
+    public String listPosts(ModelMap map) {
+        List<Post> post = postRepository.getAllPosts();
+        map.put("title", "Blog Post 1");
+        map.put("posts", post);
+        return "home";
+    }
 }
